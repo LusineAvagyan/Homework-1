5e2e974b17cb1014a51a20aa9a5b4954d8ba1948
LusineAvagyan <lusineavagyanv@gmail.com>
2022-11-09 00:55:25 +0300

    new files added

---
+++ JS/main.js
@@ -0,0 +1,25 @@
+// Task 1
+
+let i = 23;
+while (i <= 87) {
+  console.log(i);
+  i++;
+}
+
+
+// Task 2
+
+let a = 2;
+let b = 12;
+
+console.log(a**b); // 4096
+
+//Task 3
+
+let triangle = '*';
+console.log(triangle);
+while (triangle.length < 8) {
+  triangle += '*';
+  console.log(triangle); 
+}
+
